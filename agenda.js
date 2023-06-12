768578a91c7b8ec65633074b742be40bbcb06350
vasanthkumar10 <vasizebron10@gmail.com>
2023-06-12 21:38:01 +0530

    npm and node

---
+++ agenda.js
@@ -0,0 +1,14 @@
+/**
+ * npm, node, webpack, dom reconcilliation, JSX, class based components
+ * life cycle methods, functional components, props
+ */
+
+// NPM -> Node Package Manager
+// Node -> environment to run js outside browser
+// threads and process -> LIBUV -> C++
+// Node -> by default 4 workers -> go upto n no of workers
+// n -> no of cpus in a system
+
+const vasanth = require("vasanth-constant");
+
+console.log(vasanth.add(10, 20));
---
+++ vasanth-constant/index.js
@@ -0,0 +1,13 @@
+API_KEY = "abcde12345";
+
+function add(n1, n2) {
+  return n1 + n2;
+}
+
+function sub(n1, n2) {
+  return n1 - n2;
+}
+
+module.exports = {
+  add,
+};
